6bad61c87ccb39710a331e7388fa8c4f60d26252
Atul Sharma <83007914+Atulsharma428@users.noreply.github.com>
2023-08-25 15:17:48 +0530

    code

---
+++ GiftCarpet.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+#include<string.h>
+#define MAX 100
+using namespace std;
+struct GiftCarpet
+{
+    string str;
+};
+
+int main(){
+bool ans;
+int t;
+cin>>t;
+for (int i = 0; i < t; i++)
+    {
+        int n, size;
+        cin>>n>>size;
+        for (int j = 0; j < n; j++)
+        {
+            GiftCarpet strs[MAX];
+            getline(cin, strs[i].str);   
+            
+        }
+    }
+}
